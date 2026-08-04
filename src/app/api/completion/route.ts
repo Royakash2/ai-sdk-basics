f8dbb9c43c978544afc6d9a649c27945775c99d4
Royakash2 <yahmeakash@gmail.com>
2026-08-04 20:42:51 +0600

    feat: improve error handling in POST request for AI text generation

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -2,14 +2,19 @@ import { generateText } from "ai";
 import { google } from "@ai-sdk/google";
 
 export async function POST(req: Request) {
+  try {
     const { prompt } = await req.json();
 
     const { text } = await generateText({
-        model: google("gemini-2.5-flash"),
-        prompt: prompt,
-    })
+      model: google("gemini-2.5-flash"),
+      prompt: prompt,
+    });
 
     return Response.json({
-        text
-    })
+      text,
+    });
+  } catch(error) {
+    console.log("Error sentence: ", error);
+    return Response.json({error: " failed to generate text"}, {status: 500})
+  }
 }
