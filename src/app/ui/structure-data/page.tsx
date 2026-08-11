dea4ef3769e1b7d1084ba438f2a5654f19ee7832
Royakash2 <yahmeakash@gmail.com>
2026-08-11 22:34:28 +0600

    feat: add initial StructureData component with state management

---
+++ src/app/ui/structure-data/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useState } from "react"
+
+export default function StructureData() {
+    const [dishName,setDishName]= useState("");
+
+     return <div>
+
+     </div>
+    
+}
